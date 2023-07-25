67062c39a55a3bb45795ae3be58e8ed4b662b42a
whlte11 <kvlad3876@gmail.com>
2023-07-25 19:48:01 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+
+import { Wrapper, Header } from './App.styled';
+import { ContactForm } from './contactForm/ContactForm';
+import { Filter } from './filter/Filter';
+import { ContactList } from './contactList/ContactList';
+const contactsKey = 'contacts';
+
+export function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem(contactsKey)) ?? []
   );
-};
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem(contactsKey, JSON.stringify(contacts));
+  }, [contacts]);
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  const addContact = (name, number) => {
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    setContacts(prevContacts => [contact, ...prevContacts]);
+  };
+
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const getFiltredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filtredContacts = getFiltredContacts();
+  return (
+    <Wrapper>
+      <Header>Phonebook</Header>
+      <ContactForm onSubmit={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList
+        filtredContacts={filtredContacts}
+        onDeleteContact={deleteContact}
+      />
+    </Wrapper>
+  );
+}
